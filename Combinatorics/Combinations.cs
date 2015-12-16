666e0d1613765233be43d72aff189afeacf87b6f
Наталья <nat.burdukova@gmail.com>
2015-12-16 13:09:44 +0600

    Add combinations

---
+++ Combinatorics/Combinations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorics
+{
+    public class Combinations
+    {
+        private static List<string> listCombination = new List<string>();
+
+        public List<string> CreateCombinations(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+                AddCombination(str, str[i].ToString(), i + 1);
+            return SortList(listCombination, str.Length);
+        }
+
+        private static void AddCombination(string str, string current, int lenghtCombination)
+        {
+            string combination;
+            while (lenghtCombination < str.Length)
+            {
+                combination = current;
+                combination += str[lenghtCombination++];
+                listCombination.Add(combination);
+                if (combination.Length < str.Length + 1)
+                    AddCombination(str, combination, lenghtCombination);
+            }
+        }
+
+        private List<string> SortList(List<string> curList, int length)
+        {
+            List<string> resultList = new List<string>();
+            for (int i = 2; i < length + 1; i++)
+            {
+                var res = curList.FindAll(x => x.Length == i);
+                foreach (var cur in res)
+                    resultList.Add(cur);
+            }
+            return resultList;
+        }
+    }
+}
---
+++ Combinatorics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinatorics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1234567";
+            Combinations com = new Combinations();
+            var list = com.CreateCombinations(str);
+            foreach (var cur in list)
+                Console.WriteLine(cur);
+            Console.ReadKey();
+        }
+    }
+}
